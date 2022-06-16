fca35f65d847f750d225dc11db221681994c8af9
Fábio Henrique Carmona <f.h.carmona@outlook.com>
2022-06-16 19:54:14 -0300

    0.5 - Map, Lots and Buildings

---
+++ media/maps/smis/media/maps/smis/spawnpoints.lua
@@ -0,0 +1,9 @@
+function SpawnPoints()
+    return {
+
+        unemployed = {
+            { worldX = 39, worldY = 24, posX = 134, posY = 108, posZ = 0 },
+        },
+
+    }
+end
---
+++ output/spawnpoints.lua
@@ -0,0 +1,9 @@
+function SpawnPoints()
+    return {
+
+        unemployed = {
+            { worldX = 36, worldY = 45, posX = 61, posY = 102, posZ = 0 },
+        },
+
+    }
+end
