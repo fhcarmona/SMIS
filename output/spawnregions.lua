fbf8c6a70758a452bce2bbfb5ed99249ab6cf35f
Fábio Henrique Carmona <f.h.carmona@outlook.com>
2022-06-18 19:00:48 -0300

    0.6 - Steam Upload
    New Houses, Sidewalk.

--- output/spawnregions.lua
+++ output/spawnregions.lua
@@ -3,6 +3,6 @@ function SpawnRegions()
         { name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
         { name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
         { name = "Bedford Falls", file = "media/maps/BedfordFalls/spawnpoints.lua" },
-        { name = "Smis", file = "media/maps/smis/spawnpoints.lua" },
+        { name = "Smis", file = "mods/smis/media/maps/smis/spawnpoints.lua" },
     }
 end
---
+++ smis/Contents/mods/smis/media/maps/smis/spawnregions.lua
@@ -0,0 +1,8 @@
+function SpawnRegions()
+    return {
+        { name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+        { name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+        { name = "Bedford Falls", file = "media/maps/BedfordFalls/spawnpoints.lua" },
+        { name = "Smis", file = "media/maps/smis/spawnpoints.lua" },
+    }
+end
